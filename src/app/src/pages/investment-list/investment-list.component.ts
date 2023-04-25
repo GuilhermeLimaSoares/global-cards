ea2c0afc46a62fdc7189e24a367e61fa1587c9aa
GuilhermeLimaSoares <guilhermelimasoares9@gmail.com>
2023-04-25 18:44:18 -0300

    Integrating API in investment list page

--- src/app/src/pages/investment-list/investment-list.component.ts
+++ src/app/src/pages/investment-list/investment-list.component.ts
@@ -1,23 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { InvestmentsService } from '../../services/investments.service';
+import {InvestmentList} from'../../models/investment-list';
 
-export interface PeriodicElement {
-  name: string;
-  totalBalance: number;
-  goal: string;
-};
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  {name: 'Hydrogen', totalBalance: 1.0079, goal: 'H'},
-  {name: 'Helium', totalBalance: 4.0026, goal: 'He'},
-  {name: 'Lithium', totalBalance: 6.941, goal: 'Li'},
-  {name: 'Beryllium', totalBalance: 9.0122, goal: 'Be'},
-  {name: 'Boron', totalBalance: 10.811, goal: 'B'},
-  {name: 'Carbon', totalBalance: 12.0107, goal: 'C'},
-  {name: 'Nitrogen', totalBalance: 14.0067, goal: 'N'},
-  {name: 'Oxygen', totalBalance: 15.9994, goal: 'O'},
-  {name: 'Fluorine', totalBalance: 18.9984, goal: 'F'},
-  { name: 'Neon', totalBalance: 20.1797, goal: 'Ne'},
+const ELEMENT_DATA: InvestmentList[] = [
+  {
+  acoes: [],
+  indicadorCarencia: 'N',
+  nome: 'INVESTIMENTO I',
+  objetivo: 'Minha aposentadoria',
+  saldoTotal: 39321.29
+  },
 ];
 
 @Component({
@@ -27,14 +19,14 @@ const ELEMENT_DATA: PeriodicElement[] = [
 })
 
 export class InvestmentListComponent implements OnInit{
-  displayedColumns: string[] = ['name', 'goal', 'totalBalance', ];
-  dataSource = ELEMENT_DATA;
+  displayedColumns: string[] = ['nome', 'objetivo', 'saldoTotal', ];
+  dataSource: InvestmentList[] = ELEMENT_DATA;
 
   constructor(private service: InvestmentsService) {}
 
   ngOnInit(): void {
     this.service.listInvestments().subscribe((data) => {
-      console.log('data investment:', data);
+      this.dataSource = data?.response?.data?.listaInvestimentos;
     })
   }
 }
