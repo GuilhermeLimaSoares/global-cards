ea80fa968dc83f951354f8370cd19c2337a7e6b9
GuilhermeLimaSoares <guilhermelimasoares9@gmail.com>
2023-04-27 01:03:29 -0300

    Add real currency pipe on custom ransom page

--- src/app/src/pages/custom-ransom/custom-ransom.component.ts
+++ src/app/src/pages/custom-ransom/custom-ransom.component.ts
@@ -115,4 +115,8 @@ export class CustomRansomComponent implements OnInit, DoCheck {
       initialValue
     );
   }
+
+  calculePercentual(percentual: number, totalBalance: number): number {
+    return totalBalance * (percentual/100);
+  }
 }
---
+++ src/app/src/utils/index.ts
@@ -0,0 +1,3 @@
+export function calculePercentual(percentual: number, totalBalance: number): number {
+  return totalBalance * (percentual/100);
+}
